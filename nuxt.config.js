62428431a90d5ffa2c62212f8b710d5ef711b7c9
KarliaN <cemu9@mail.ru>
2021-05-21 11:21:08 +0300

    fav

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/logo.jpg' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
   telemetry: false,
